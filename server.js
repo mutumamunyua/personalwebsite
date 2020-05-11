dc75e072a42be9bdac7527408ea9bbe5c93bf615
Philip <mutumamunyua@gmail.com>
2020-05-11 01:24:04 -0400

    may 11th 2020

--- server.js
+++ server.js
@@ -31,6 +31,10 @@ const Message = mongoose.model('message', new Schema({
     },
     message: {
         type: String
+    }, 
+    date: {
+        type: Date,
+        default: Date.now
     }
 }));
 // setup environment variables
